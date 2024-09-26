801f2aeb76403b52a0aef3992d3a2ba6d75a0b85
Gopi-Vuta <gopisaikrishna.vuta@gmail.com>
2024-09-26 20:47:53 +0530

    gopi, revathy | Update Home page

--- src/libs/home/home.tsx
+++ src/libs/home/home.tsx
@@ -1,46 +1,102 @@
-import React from 'react'
-import ProtectedSection from '../auth/protected-section'
-import { useAuth } from '../auth/use-auth-hook'
-import HeroImage from '../../assets/images/hero.svg'
-import { Typography } from '@mui/material'
+import React from "react";
+import ProtectedSection from "../auth/protected-section";
+import { useAuth } from "../auth/use-auth-hook";
+import HeroImage from "../../assets/images/hero.svg";
+import { Button, Typography } from "@mui/material";
+import { useNavigate, useNavigation } from "react-router-dom";
 
 const Home = () => {
-  const { username } = useAuth();
-  const renderUserGreetingSection = () => {
-    return <ProtectedSection>
-      {<div className='mb-8 font-mono text-2xl font-bold text-main-light'>
-        <h6>Hi {username}, Welcome!</h6>
-      </div>}
-    </ProtectedSection>
-  }
+  const { username, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  const userGreeting: string = isAuthenticated
+    ? `HI ${username.toUpperCase()}, `
+    : "";
 
   return (
-    <div className='flex justify-center items-center'>
-      <div className='flex w-full'>
-        <div className='w-1/2'>
-          <div className='ml-24 mt-48'>
-            {renderUserGreetingSection()}
-            <div className='font-extrabold text-5xl text-main-medium'>
-              <h3>Laser focus, high impact</h3>
+    <div className="flex justify-center items-center flex-col">
+      <div className="flex w-full mt-40 mb-48 px-72">
+        <div>
+          <img
+            src="https://img1.wsimg.com/isteam/stock/1YEW15Z/:/rs=w:772,h:500,cg:true,m/cr=w:772,h:500"
+            alt="Image explaining datagets"
+            width="750"
+            height="750"
+          />
+        </div>
+        <div className="flex bg-gray-1">
+          <div className="flex w-128 justify-center items-center flex-col gap-6">
+            <div className="flex">
+              <Typography>{userGreeting}WELCOME</Typography>
             </div>
-            <div className=' my-14 text-lg'>
+            <div className="flex mx-4">
+              <p className="text-5xl text-center">
+                Drive business impact from your data
+              </p>
+            </div>
+            <div className="flex">
+              <Typography>Get help from the experts</Typography>
+            </div>
+            <Button
+              variant="contained"
+              size="large"
+              onClick={() => {
+                navigate("/services");
+              }}
+            >
+              LEARN MORE
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="flex w-full px-72">
+        <div className="w-1/2">
+          <img
+            src="https://img1.wsimg.com/isteam/ip/b74283d7-df57-4724-872f-dc408d647ff5/blob-3c58943.png/:/cr=t:0%25,l:5.65%25,w:66.67%25,h:100%25/rs=w:1200,h:1200,cg:true"
+            alt="image about us"
+          />
+        </div>
+        <div className="w-1/2">
+          <div className="ml-24 mt-32">
+            <div className="flex justify-center items-center">
+              <div className="font-medium text-4xl text-main-medium">
+                <h5>Laser focus, high impact</h5>
+              </div>
+            </div>
+            <div className=" my-14 text-lg">
               <ul className="list-disc">
-                <li><b>Boutique data consultancy:</b> we are focused at the intersection of Data and Marketing + Digital Commerce</li>
-                <li><b>Industry leading solutions:</b> our founders have 20 yrs+ experience in building and refining data solutions at Fortune 50 companies</li>
-                <li><b>Measurable Impact</b> - granular attribution framework that links your data products to Revenue or other KPI lift</li>
-                <li><b>Flexible engagement model</b> - you pay 'only' when our solutions improves your KPIs or project-based fixed cost or time/labor based costing</li>
+                <li>
+                  <b>Boutique data consultancy:</b> we are focused at the
+                  intersection of Data and Marketing + Digital Commerce
+                </li>
+                <li>
+                  <b>Industry leading solutions:</b> our founders have 20 yrs+
+                  experience in building and refining data solutions at Fortune
+                  50 companies
+                </li>
+                <li>
+                  <b>Measurable Impact</b> - granular attribution framework that
+                  links your data products to Revenue or other KPI lift
+                </li>
+                <li>
+                  <b>Flexible engagement model</b> - you pay 'only' when our
+                  solutions improves your KPIs or project-based fixed cost or
+                  time/labor based costing
+                </li>
               </ul>
             </div>
           </div>
         </div>
-        <div className='w-1/2'>
-          <div className='m-14'>
-            <img src={HeroImage} alt="Image explaining datagets"/>
-          </div>
+      </div>
+      <div className="flex justify-center item-center py-24">
+        <div>
+          <Typography>
+            Copyright &copy; 2024 DATAGETS - All Rights Reserved.
+          </Typography>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      width: {
+        128: "29rem",
+      },
       colors: {
-        'main-dark': '#17153B',
-        'main-medium': '#2E236C',
-        'main-light': '#433D8B',
-        'main-pale': '#C8ACD6',
+        "main-dark": "#17153B",
+        "main-medium": "#2E236C",
+        "main-light": "#433D8B",
+        "main-pale": "#C8ACD6",
+        "gray-1": "#f2f3ff",
       },
     },
   },
   plugins: [],
-}
+};
